a0468d92153243d27c2cd16eef81f4388ea87186
Alex Trinh <alexander9821@gmail.com>
2022-03-08 21:18:40 -0800

    file read in

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, redirect, render_template, request, url_for
 
+
 app = Flask(__name__)
+
+# global variables
 
 @app.route("/", methods=["POST", "GET"])
 def landing_page():
@@ -17,17 +20,30 @@ def landing_page():
 @app.route("/manifest-input", methods=["POST", "GET"])
 def manifest_input_page():
     if request.method == "POST":
-        print(request.form)
-        if "manifest-continue" in request.form:
+        
+        f = request.files['manifest']
+        print(f)
+        if f != "":
+            return render_template("index.html")
+        else:
+            #read in file
             return redirect(url_for("main_page"))
-        elif "manifest-quit" in request.form:
-            return redirect(url_for("landing_page"))
     else:
         return render_template("manifest-input.html")
 
-@app.route("/main")
+@app.route("/main", methods=["POST", "GET"])
 def main_page():
-    return render_template("index.html")
+    if request.method == "POST":
+        file_name = request.get_json()
+        if file_name == "":
+            print("empty")
+            return render_template("manifest-input.html")
+        else:
+            #read in file
+            print(file_name)
+            return render_template("index.html")
+    else:
+        return render_template("manifest-input.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
