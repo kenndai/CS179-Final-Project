005e5d30454924be8df613199cdec2d57b56ec30
kenndai <heyitskennydai@gmail.com>
2022-03-12 21:01:01 -0800

    Initial grid expansion working

--- problem.py
+++ problem.py
@@ -1,87 +1,97 @@
-class ShipNode:
-    __slots__ = ["distance_cost", "ship_state", "parent_ship_state"]
+# class ShipNode:
+#     __slots__ = ["distance_cost", "ship_grid", "parent_ship_grid"]
 
-    def __init__(self, distance_cost, ship_state, parent_ship_state = None):
-        self.distance_cost = distance_cost
-        self.ship_state = ship_state
-        self.parent_ship_state = parent_ship_state
+#     def __init__(self, distance_cost, ship_grid, parent_ship_grid = None):
+#         self.distance_cost = distance_cost
+#         self.ship_grid = ship_grid
+#         self.parent_ship_grid = parent_ship_grid
+import copy
 
+from sqlalchemy import column
 class ShipProblem:
 
-    __slots__ = ["initial_state", "total_moves"]
+    __slots__ = ["grid", "parent", "distance_cost", "top_containers"]
 
-    def __init__(self, consensed_manifest = None):
-        self.initial_state = [
+    def __init__(self, distance_cost = 0, grid = None, parent = None):
+        self.distance_cost = distance_cost
+        # self.grid = grid
+        self.grid = [
             {'coordinate': ['01', '01'], 'weight': 0, 'text': 'NAN'}, 
             {'coordinate': ['01', '02'], 'weight': 0, 'text': 'NAN'}, 
             {'coordinate': ['01', '06'], 'weight': 2500, 'text': 'John Deere Parts (call Sue at Ohio office)'}, 
-            {'coordinate': ['01', '11'], 'weight': 0, 'text': 'NAN'}, 
-            {'coordinate': ['01', '12'], 'weight': 0, 'text': 'NAN'}, 
             {'coordinate': ['02', '01'], 'weight': 6000, 'text': 'John Deere Oversized Tires(call Sue at Ohio office)'},
             {'coordinate': ['02', '06'], 'weight': 6000, 'text': 'John Deere Oversized Tires(call Sue at Ohio office)'},
             {'coordinate': ['03', '01'], 'weight': 6000, 'text': 'John Deere Oversized Tires(call Sue at Ohio office)'},
             {'coordinate': ['02', '01'], 'weight': 6000, 'text': 'John Deere Oversized Tires(call Sue at Ohio office)'},
-            {'coordinate': ['05', '01'], 'weight': 6000, 'text': 'John Deere Oversized Tires(call Sue at Ohio office)'}]
+            {'coordinate': ['05', '01'], 'weight': 99999, 'text': 'John Deere Oversized Tires(call Sue at Ohio office)'}]
+        self.parent = parent
 
-        # self.initial_state = condensed_manifest
+        self.top_containers = self.set_top_containers()
 
-        ## set total_moves to 1 so operations begin with an odd move
-        self.total_moves = 1
-        
+    ## finds and sets the top containers for all columns
+    def set_top_containers(self):
+        ship_grid = self.grid
+        top_containers = {
+            1: None,
+            2: None,
+            3: None,
+            4: None,
+            5: None,
+            6: None
+        }
 
-    # define operations to move a container to another column
-    ## Odd moves: pick up
-    ## Even moves: place down
+        for container in ship_grid:
+            curr_x = int(container["coordinate"][1])
+            curr_y = int(container["coordinate"][0])
 
-    ## position crane over first column
-    def move_crane_first(self, ship_state : ShipNode.ship_state):
-        new_states = []
-        top_container = None
-        curr_top_y = -1
-        for container in ship_state:
-            x_coord = int(container["coordinate"][1])
-            if x_coord != 1: continue
-            y_coord = int(container["coordinate"][0])
+            top_x_container = top_containers[curr_x] 
+            if top_containers[curr_x] == None or curr_y > int(top_x_container["coordinate"][0]):
+                top_containers[curr_x] = container
 
-            ## find the top container in the first column
-            ## if column is EMPTY or highest contianer is NAN, skip
-            if container["text"] == "NAN": continue
-            if y_coord > curr_top_y:
-                curr_top_y = y_coord
-                top_container = container
+        return top_containers
 
-        if curr_top_y == -1: return
-        for i in range(2, 5):
-            new_states.append()
+    ## position crane over a column and expand
+    ## returns a list of grids
+    def move_crane(self, column_num):
+        new_grids = []
 
-        return top_container
+        # return if top container in a column is an unused slot or "NAN" container 
+        if self.top_containers[column_num] == None or self.top_containers[column_num]["text"] == "NAN": 
+            print("no container to grab")
+            return [[]]
 
-    ## position crane over first column
-    def move_crane_second(self, ship_state : ShipNode.ship_state):
+        # get the top container in the first column
+        top_container = self.top_containers[column_num]
+        # find the index of the container in the grid
+        index_in_grid = self.grid.index(top_container)
 
-        return
+        for i in range(1, 7):
+            # skip the column you're moving away from
+            if i == column_num: continue
 
-    ## position crane over first column
-    def move_crane_third(self, ship_state : ShipNode.ship_state):
+            # makes a deepcopy of list, i hate python
+            new_grid = copy.deepcopy(self.grid)
 
-        return
+            # get top container of the i-th column
+            i_top_container = self.top_containers[i]
 
-    ## position crane over first column
-    def move_crane_fourth(self, ship_state : ShipNode.ship_state):
-
-        return
-
-
-ship_state = [
-            {'coordinate': ['01', '01'], 'weight': 0, 'text': 'NAN'}, 
-            {'coordinate': ['01', '02'], 'weight': 0, 'text': 'NAN'}, 
-            {'coordinate': ['01', '06'], 'weight': 2500, 'text': 'John Deere Parts (call Sue at Ohio office)'}, 
-            {'coordinate': ['01', '11'], 'weight': 0, 'text': 'NAN'}, 
-            {'coordinate': ['01', '12'], 'weight': 0, 'text': 'NAN'}, 
-            {'coordinate': ['02', '01'], 'weight': 6000, 'text': 'John Deere Oversized Tires(call Sue at Ohio office)'},
-            {'coordinate': ['02', '06'], 'weight': 6000, 'text': 'John Deere Oversized Tires(call Sue at Ohio office)'}]
+            # place container in the first row of the i-th column
+            if i_top_container == None:
+                new_grid[index_in_grid]["coordinate"] = [1, i]
+            # place container one higher than the top container in the i-th column
+            else: 
+                y_coord = int(i_top_container["coordinate"][0])
+                new_grid[index_in_grid]["coordinate"] = [y_coord + 1, i]    
+            
+            new_grids.append(new_grid)
+        return new_grids
 
 problem2 = ShipProblem()
-ship_problem = problem2.initial_state
 
-print(problem2.move_crane_first(ship_problem))
+# for container in problem2.top_containers.values():
+#     print(container)
+
+for grid in problem2.move_crane(6):
+    for container in grid:
+        print(container)
+    print()
